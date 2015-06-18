f37d0b421422c57272b010b9c9017711f6833843
Marshall Bowers <mbowers@gravic.com>
2015-06-18 08:59:20 -0400

    Improve archive directory structure

--- archiver.py
+++ archiver.py
@@ -1,16 +1,18 @@
+import os
 import tarfile
 from datetime import datetime
 
 class Archiver(object):
-    def __init__(self, site_slug, input_dir):
+    def __init__(self, site_slug, input_dir, output_dir):
         self.site = site_slug
         self.input_dir = input_dir
+        self.output_dir = os.path.abspath(output_dir)
 
     def archive(self):
         timestamp = str(datetime.now()).split('.')[0].replace(' ', '_')
-        archive_name = '{0}_{1}'.format(self.site, timestamp)
-        archive_path = archive_name
+        archive_name = '{0}_{1}.tar.bz2'.format(self.site, timestamp)
+        archive_path = os.path.join(self.output_dir, archive_name)
 
-        tar = tarfile.open(archive_name, 'w:bz2')
+        tar = tarfile.open(archive_path, 'w:bz2')
         tar.add(self.input_dir)
         tar.close()
--- tasks.py
+++ tasks.py
@@ -1,14 +1,18 @@
+import os
 from app import celery
 from compiler import Compiler
 from archiver import Archiver
 
 @celery.task
 def deploy(slug, testing_url, production_url):
-    compiler = Compiler('./test/', testing_url, production_url)
+    build_dir = os.path.join('./dist/build/', slug)
+    archive_dir = os.path.join('./dist/archive/', slug)
+
+    compiler = Compiler(build_dir, testing_url, production_url)
 
     compiler.compile()
 
-    archiver = Archiver(slug, './test/')
+    archiver = Archiver(slug, build_dir, archive_dir)
 
     archiver.archive()
 
