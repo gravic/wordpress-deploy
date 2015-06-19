af35b3b0b0b43ee0dd42535d748339fa479191d5
Marshall Bowers <mbowers@gravic.com>
2015-06-19 10:55:10 -0400

    Create admin account with the database

--- db/init.py
+++ db/init.py
@@ -1,3 +1,15 @@
-from app import db
+from app import db, User
 
 db.create_all()
+
+user = User(
+    'admin',
+    'password',
+    'Admin',
+    'Account'
+)
+
+user.is_admin = True
+
+db.session.add(user)
+db.session.commit()
